bffa4bddf9346b75d7c50af658e63405fa097b1d
Nadim Tuhin <nadimtuhin@gmail.com>
2016-11-30 23:36:55 +0600

    insertionsort bugfix

--- src/sort/insertionSort.js
+++ src/sort/insertionSort.js
@@ -1,5 +1,18 @@
 function insertionSort(collection) {
     const result = collection.slice();
+    const total = collection.length;
+
+    for (let indexToSort = 1; indexToSort < total; indexToSort++) {
+        for (let ii = indexToSort; ii > 0; ii--) {
+            if(result[ii] < result[ii - 1]) {
+                const small = result[ii];
+                const large = result[ii-1];
+
+                result[ii-1] = small;
+                result[ii] = large;
+            }
+        }
+    }
 
     return result;
 }
