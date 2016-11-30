71ab7bd68454d18e867158d98a812755d93e2e1c
Nadim Tuhin <nadimtuhin@gmail.com>
2016-12-01 00:41:59 +0600

    typo fix

--- src/sort/bubbleSort.spec.js
+++ src/sort/bubbleSort.spec.js
@@ -1,6 +1,6 @@
 const bubbleSort = require('./bubbleSort');
 
-test('bubbleSort should sort an ordered array', () => {
+test('bubbleSort should sort an unordered array', () => {
     const collection = [3, 4, 63, 2, 1, 5, 7];
     const expectedResult = [1, 2, 3, 4, 5, 7, 63];
     const result = bubbleSort(collection);
--- src/sort/insertionSort.spec.js
+++ src/sort/insertionSort.spec.js
@@ -1,6 +1,6 @@
 const insertionSort = require('./insertionSort');
 
-test('insertionSort should sort an ordered array', () => {
+test('insertionSort should sort an unordered array', () => {
     const collection = [3, 4, 63, 2, 1, 5, 7];
     const expectedResult = [1, 2, 3, 4, 5, 7, 63];
     const result = insertionSort(collection);
