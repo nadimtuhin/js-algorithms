32486bacb6bf0ce0364eec5a6d24cf250ae6a612
Nadim Tuhin <nadimtuhin@gmail.com>
2016-12-02 19:28:24 +0600

    working on mergesort

--- src/sort/mergeSort.js
+++ src/sort/mergeSort.js
@@ -1,24 +1,5 @@
-function selectionSort(collection) {
-    const total = collection.length;
-    const result = collection.slice();
+function mergeSort(collection) {
 
-    for (let indexToSort = 0; indexToSort < total; indexToSort++) {
-        let lowestIndex = indexToSort;
-
-        for (let ii = indexToSort + 1; ii < total; ii++) {
-            if (result[lowestIndex] > result[ii]) {
-                lowestIndex = ii;
-            }
-        }
-
-        const small = result[lowestIndex];
-        const large = result[indexToSort];
-
-        result[indexToSort] = small;
-        result[lowestIndex] = large;
-    }
-
-    return result;
 }
 
-module.exports = selectionSort;
+module.exports = mergeSort;
