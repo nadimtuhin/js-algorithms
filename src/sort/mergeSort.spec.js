b6fcdbdb7f5353d1c056145ebeb3437c773e6cf0
Nadim Tuhin <nadimtuhin@gmail.com>
2016-12-02 19:27:46 +0600

    updated mergesort spec

--- src/sort/mergeSort.spec.js
+++ src/sort/mergeSort.spec.js
@@ -1,9 +1,9 @@
-const bubbleSort = require('./bubbleSort');
+const mergeSort = require('./mergeSort');
 
-test('bubbleSort should sort an unordered array', () => {
+test('mergeSort should sort an unordered array', () => {
     const collection = [3, 4, 63, 2, 1, 5, 7];
     const expectedResult = [1, 2, 3, 4, 5, 7, 63];
-    const result = bubbleSort(collection);
+    const result = mergeSort(collection);
 
     expect(result).toEqual(expectedResult);
 });
